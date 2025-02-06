194ae9fcdd5766dbfc205c4e59730a5c44897620
YuriiVern <yuriy.vernygorov@gmail.com>
2025-02-06 23:19:07 +0200

    add random secret generator

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data "http" "my_ip" {
+  url = "https://checkip.amazonaws.com"
+}
+
 resource "aws_security_group" "default_local_host_sg" {
   name        = "default_local_host_sg"
   description = "Security group for SSH and HTTP from local host"
@@ -21,21 +25,21 @@ resource "aws_security_group" "default_local_host_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]  
+    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]  
   }
 
   egress {
@@ -51,8 +55,24 @@ resource "aws_key_pair" "private_ssh" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
+data "aws_secretsmanager_random_password" "pswd_generator" {
+  password_length  = 50
+  exclude_numbers = true
+}
+
+resource "aws_secretsmanager_secret" "my_secret_terraform" {
+  name = var.secret_key
+  description = "My secrted container from terraform"
+}
+
+resource "aws_secretsmanager_secret_version" "my_secret_value" {
+  secret_id     = aws_secretsmanager_secret.my_secret_terraform.id
+  secret_string = data.aws_secretsmanager_random_password.pswd_generator.random_password
+}
+
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-04b4f1a9cf54c11d0"
+  ami           = var.ami_id
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   security_groups = [aws_security_group.default_local_host_sg.name]
